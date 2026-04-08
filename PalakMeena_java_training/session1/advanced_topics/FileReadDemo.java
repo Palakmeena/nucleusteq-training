60b5ab756c4f708e6d02a00648e8766150569850
Palak Meena <palakmeena14@gmail.com>
2026-04-08 20:59:00 +0530

    feat: implement efficient file reading using BufferedReader and try-with-resources

---
+++ PalakMeena_java_training/session1/advanced_topics/FileReadDemo.java
@@ -0,0 +1,26 @@
+package PalakMeena_java_training.session1.advanced_topics;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+//Example of File I/O in Java to read content from a file and display it on the console
+
+public class FileReadDemo {
+    public static void main(String[] args) {
+      
+        String fileName = "data.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            System.out.println("--- Reading File Content ---");
+            
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
