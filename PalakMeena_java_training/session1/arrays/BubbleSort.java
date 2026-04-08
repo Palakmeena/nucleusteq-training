229bebed728e415ee45bf06cf0b78f075dd826cc
Palak Meena <palakmeena14@gmail.com>
2026-04-08 17:31:42 +0530

    feat: sort array in ascending order using bubble sort algorithm

---
+++ PalakMeena_java_training/session1/arrays/BubbleSort.java
@@ -0,0 +1,58 @@
+package PalakMeena_java_training.session1.arrays;
+
+import java.util.Scanner;
+
+/**
+ * Program to sort an array in ascending order using Bubble Sort.
+ */
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int size = scanner.nextInt();
+
+        int[] numbers = new int[size];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        System.out.print("Before sorting: ");
+        printArray(numbers);
+
+        bubbleSort(numbers);
+
+        System.out.print("After sorting:  ");
+        printArray(numbers);
+
+        scanner.close();
+    }
+}
