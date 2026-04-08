788a05efcff2c1b19e5b7f1dcc372e911d4b2fa9
Palak Meena <palakmeena14@gmail.com>
2026-04-08 15:42:38 +0530

    feat: add even-odd checker using modulo operator

---
+++ PalakMeena_java_training/session1/basics/EvenOddChecker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+/**
+ * Program to check whether a given number is even or odd.
+ */
+public class EvenOddChecker {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+ 
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+ 
+        scanner.close();
+    }
+}
+ 
