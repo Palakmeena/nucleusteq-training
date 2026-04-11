443dbe7263173cb6608ae2f7daff89359fc60e17
Palak Meena <palakmeena14@gmail.com>
2026-04-11 19:33:13 +0530

    test: Add application test class for Spring Boot integration testing

---
+++ PalakMeena_java_training/session2/spring-core-assignment/src/test/java/com/palak/springcoreassignment/SpringCoreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palak.springcoreassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
