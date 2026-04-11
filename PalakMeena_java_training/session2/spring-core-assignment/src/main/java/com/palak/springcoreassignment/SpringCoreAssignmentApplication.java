6fa4efcd9d853062fd7cea13738368da1601b462
Palak Meena <palakmeena14@gmail.com>
2026-04-11 19:17:19 +0530

    feat: Initialize Spring Boot project with Spring Initializr

---
+++ PalakMeena_java_training/session2/spring-core-assignment/src/main/java/com/palak/springcoreassignment/SpringCoreAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.palak.springcoreassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreAssignmentApplication.class, args);
+	}
+
+}
