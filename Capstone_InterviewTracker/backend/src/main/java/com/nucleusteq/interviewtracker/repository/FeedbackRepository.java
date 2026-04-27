6eea3c9e5fb397b7a4cd6ea24a0d0e49b89b3f8a
Palak Meena <palakmeena14@gmail.com>
2026-04-27 17:08:32 +0530

    feat: add FeedbackRepository

---
+++ Capstone_InterviewTracker/backend/src/main/java/com/nucleusteq/interviewtracker/repository/FeedbackRepository.java
@@ -0,0 +1,13 @@
+package com.nucleusteq.interviewtracker.repository;
+
+import com.nucleusteq.interviewtracker.entity.Feedback;
+import com.nucleusteq.interviewtracker.entity.Interview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    List<Feedback> findByInterview(Interview interview);
+}
