42bbd523ca7652ff7182b9758136708c4a09f2fe
Palak Meena <palakmeena14@gmail.com>
2026-04-27 17:10:28 +0530

    feat: add CandidateProfileService

---
+++ Capstone_InterviewTracker/backend/src/main/java/com/nucleusteq/interviewtracker/repository/CandidateProfileRepository.java
@@ -0,0 +1,13 @@
+package com.nucleusteq.interviewtracker.repository;
+
+import com.nucleusteq.interviewtracker.entity.CandidateProfile;
+import com.nucleusteq.interviewtracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CandidateProfileRepository extends JpaRepository<CandidateProfile, Long> {
+    Optional<CandidateProfile> findByUser(User user);
+}
