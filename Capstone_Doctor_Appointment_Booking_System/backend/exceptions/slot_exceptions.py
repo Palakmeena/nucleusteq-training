adb73d871116208d5b0af0ae6cc9107d02ba5230
Palak Meena <palakmeena14@gmail.com>
2026-07-02 20:40:48 +0530

    feat(exceptions): add slot management exceptions

---
+++ Capstone_Doctor_Appointment_Booking_System/backend/exceptions/slot_exceptions.py
@@ -0,0 +1,21 @@
+"""Custom slot exceptions."""
+
+
+class SlotNotFoundException(Exception):
+    """Raised when a slot cannot be found."""
+
+
+class SlotAlreadyBookedException(Exception):
+    """Raised when the slot has already been booked."""
+
+
+class SlotOverlapException(Exception):
+    """Raised when two slots overlap."""
+
+
+class InvalidSlotTimeException(Exception):
+    """Raised when slot timing is invalid."""
+
+
+class SlotCannotBeDeletedException(Exception):
+    """Raised when a slot cannot be deleted because it is booked."""
